db01b0c97494946371a2198d7cce4d026ca192e7
ups3tspaghet <jeffhwu2004@gmail.com>
2023-08-07 12:42:22 -0400

    more scripts

--- AnagramSolverScript.cpp
+++ AnagramSolverScript.cpp
@@ -1,14 +1,29 @@
-// Example 1.1
 #include <iostream>
-#include <string>
-
+using std::cin;
 using std::cout;
 using std::endl;
-using std::string;
+#include <cmath>
+using std::pow;
 
+int increment(int val, int incr = 1) {
+  val += incr;
+  return val;
+}
 
+// calculate ax^2 + bx + c
+double eval_poly(double x, double a = 1.0, double b = 1.0, double c = 1.0) {
+  return a * pow(x, 2) + b * x + c;
+}
 
 int main() {
-  cout<< "shit";
+  int my_int = 27;
+  cout << increment(my_int, 5) << endl;
+  cout << increment(my_int) << endl;
+  ;
+  // cout << "Val is now:"<<increment();
 
+  // 1*(1^2) + 1*1 + 1
+  cout << eval_poly(1) << endl;
+  // -2*(2^2) + 3*2 + 1
+  cout << eval_poly(2, -2, 3) << endl;
 }
